23f6683aa57331f195daabc162167ff2531c9903
jongohlke <jongohlke@gmail.com>
2020-01-23 09:28:22 -0500

    Initial project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactRogue from './components/ReactRogue';
+
+const App = () => (
+  <div className='App'>
+    <ReactRogue width={40} height={40} tilesize={16} />
+  </div>
+) 
+
+export default App;
